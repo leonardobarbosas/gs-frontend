589710eb2e21bc42578d2c5a2a899827fb466372
leonardobarbosas <leonardobarbosasantos@gmail.com>
2024-11-22 15:44:58 -0300

    pagina sobre o projeto concluída

--- gs/src/app/sobre-projeto/page.tsx
+++ gs/src/app/sobre-projeto/page.tsx
@@ -1,9 +1,26 @@
 "use client";
 
+import Image from "next/image";
+import imgRenovavel from "@/img/renovavel.png";
+
 export default function SobreProjeto() {
   return (
-    <div>
-      <h1>teste</h1>
+    <div className="w-full flex justify-around">
+      <Image
+        src={imgRenovavel}
+        width={700}
+        alt="Imagem Renovável"
+        className="shadow-2xl rounded-lg"
+      />
+      <p className="text-3xl  t-color2 w-1/3">
+        A Lf Energy é uma plataforma inovadora que conecta pessoas e empresas às
+        melhores soluções de energia renovável. Com um diagnóstico
+        personalizado, comparação de fornecedores e agendamento de consultorias,
+        tornamos o processo de adoção de energias limpas simples, prático e
+        transparente. Transforme sua relação com a energia e contribua para um
+        futuro sustentável. Na Lf Energy, a sustentabilidade está ao seu
+        alcance!
+      </p>
     </div>
   );
 }
